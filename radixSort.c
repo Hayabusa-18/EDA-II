8ffb246136d5def7097aee6684d922471a329798
Hayabusa-18 <osvaldo_serrcu@hotmail.com>
2021-09-24 02:36:35 -0500

    Update radixSort.c

--- radixSort.c
+++ radixSort.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<iso646.h>
 
+
+// list: la lista a ordenar
+// tam: el número de elementos en la lista
+// numPos: el número de valores posicionales: 10 tiene 2 valores, 100 tiene 3 valores,
+// 1000 tiene 4 valores, y así sucesivamente
+// radix: la base numérica. Si en un problema en particular la base siempre será 10,
+// entonces podríamos quitar este argumento
+
+radixSort(size_t list[], TAM, numPos, radix){
+	//Crear tantas colas como Radix indique
+
+	for
+}
